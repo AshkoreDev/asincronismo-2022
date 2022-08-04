c65b6b166f9f483a7854ee7f12f7c013617f7ceb
Aracely Alba <aracelyalba8@gmail.com>
2022-08-03 23:50:48 -0430

    Added all promises

--- src/promises/challenge.js
+++ src/promises/challenge.js
@@ -12,3 +12,23 @@ fetchData(`${API}/products`)
 	.then((response) => response.json())
 	.then((products) => console.log(products))
 	.catch((error) => console.log(error))
+
+
+fetchData(`${API}/products`)
+	// Mostrar los productos
+	.then((response) => response.json())
+	.then((products) => {
+		console.log(products);
+		return fetchData(`${API}/products/${products[0].id}`)
+	})
+	// Mostrar el titulo del producto
+	.then((response) => response.json())
+	.then((product) => {
+		console.log(products.title);
+		return fetchData(`${API}/categories/${product.category.id}`)
+	})
+	// Mostrar el nombre de la categoria
+	.then((response) => response.json())
+	.then((category) => console.log(category.name))
+	.catch((error) => console.log(error))
+	.finally(() => console.log('Finally.'));
