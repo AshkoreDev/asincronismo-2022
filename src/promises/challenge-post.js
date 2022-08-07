44b67eb6718fe01ea19d81c04ad4fe980d7ccd82
Aracely Alba <aracelyalba8@gmail.com>
2022-08-06 23:47:55 -0430

    Added promise fetch post

---
+++ src/promises/challenge-post.js
@@ -0,0 +1,21 @@
+
+import fetch from 'node-fetch';
+
+const API = 'https://api.escuelajs.co/api/v1';
+
+function postData(urlApi, data) {
+	
+	const response = fetch(urlApi, {
+
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'same-origin',
+		header: {
+			'Content-type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	});
+
+	return response;
+}
+
